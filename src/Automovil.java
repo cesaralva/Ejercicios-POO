c54e6440920f72072cda260dbae855dffecee930
Cesar Alva <66078322+cesaralva@users.noreply.github.com>
2023-04-12 09:16:37 -0500

    first commit

---
+++ src/Automovil.java
@@ -0,0 +1,134 @@
+public class Automovil { // clase entidad representa la clase de la application las clases se crean con mayusculas tipo oracion
+    private String fabricante = "nissan";
+    private String modelo;
+    private String color = "Verde";
+    private double cilindrada;
+    private int capacidadtanque = 40;
+    //ahora instanciamos
+
+//funciones de los metodos // dentro de los parametros o parentesis entarn la clase metodo
+    //no se debe imprimir en el metodo
+
+    ///////////constructor vacio///////////////
+    public Automovil() {
+    }
+
+    /////////////////////////////////////////
+
+    ////////////////////Re uso dul constructor///////////////////
+    public Automovil(String fabricante, String modelo, double cilindrada) {
+        this.fabricante = fabricante;
+        this.modelo = modelo;
+    }
+
+    public Automovil(String fabricante, String modelo) {
+        this.fabricante = fabricante;
+        this.modelo = modelo;
+
+    }
+
+    public Automovil(String fabricante, String modelo, String color, double cilindrada) {
+        this(fabricante, modelo);
+        this.color = color;
+
+    }
+
+    public Automovil(String fabricante, String modelo, String color, double cilindrada, int capacidadtanque) {
+        this(fabricante, modelo, color, cilindrada);
+        this.capacidadtanque = capacidadtanque;
+    }
+////////////////////////////////////////////
+
+    ////// get para obtener y set para modificar/////////
+    public String getFabricante() {
+        return fabricante;
+    }
+
+    public void setFabricante(String fabricante) {
+        this.fabricante = fabricante;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public double getCilindrada() {
+        return cilindrada;
+    }
+
+    public void setCilindrada(double cilindrada) {
+        this.cilindrada = cilindrada;
+    }
+
+    public int getCapacidadtanque() {
+        return capacidadtanque;
+    }
+
+    public void setCapacidadtanque(int capacidadtanque) {
+        this.capacidadtanque = capacidadtanque;
+    }
+
+    //////////////////////////////////////////////////////////
+    public String verDetalle() {
+        //se usa para concatena r cadenas
+        return "auto = " + this.fabricante +
+                "auto.modelo = " + this.modelo +
+                "auto.color = " + this.color +
+                "auto.cilindrada = " + cilindrada +
+                "modelo = " + modelo;
+
+    }
+
+    //otro metodo
+    public String acelerar(int rpm) {
+        return "el auto " + this.fabricante + " acelerando a " + rpm + " rpm";
+    }
+
+    public String frenar() {
+        return this.fabricante + " " + this.modelo + " frenando";
+    }
+
+    public String acerarFrenar(int rpm) {
+        String acelerar = this.acelerar(rpm);
+        String frenar = this.frenar();
+        return acelerar + "\n" + frenar;
+    }
+
+    public float calcularConsumo(int km, float porcentajeBencina) {
+        return km / (capacidadtanque * porcentajeBencina);
+    }
+
+    public float calcularConsumo(int km, int porcentajeBencina) {
+        return km / (capacidadtanque * (porcentajeBencina / 100f));
+    }
+
+
+    ///////////Principio de polimorfismo///////////////
+    /*Modificar el equals para hacer la comparacion*/
+    @Override
+    public boolean equals(Object obj) {
+
+        if (this == obj){
+            return true;
+        }
+        if (!(obj instanceof Automovil)) {
+            return false;
+        }
+        Automovil a = (Automovil) obj;
+        return (this.fabricante != null && this.modelo != null && this.fabricante.equals(a.getFabricante())
+                && this.modelo.equals(a.getModelo()));
+    }
+    ///////////////////////////////////////////////////
+}
---
+++ src/ejemploAutomovil.java
@@ -0,0 +1,43 @@
+import java.util.Date;
+
+public class ejemploAutomovil {
+    public static void main(String[] args) {
+        Automovil subaru = new Automovil("subaru", "Impresa");
+
+
+
+        subaru.setCilindrada(2.0);
+        subaru.setColor("blanco");
+
+        Automovil nissan = new Automovil("nissan", "bt-50", 2.0);
+        System.out.println("nissan = " + nissan.getFabricante());
+
+        Automovil mazda = new Automovil("mazda", "amarillo", "verde", 3.5, 60);
+
+        Automovil nissan2 = new Automovil("nissan", "bt-50", 2.0);
+        System.out.println("son iguales? " + (nissan == nissan2));
+        System.out.println("son iguales con equals? " + (nissan.equals(nissan2)));
+
+        Automovil auto = new Automovil();
+        Date fecha = new Date();
+        System.out.println("mazda.leerFabricante() = " + mazda.getFabricante());
+
+        System.out.println(subaru.verDetalle());
+        System.out.println(mazda.verDetalle());
+        System.out.println(subaru.acelerar(3000));
+        System.out.println(subaru.frenar());
+        System.out.println(nissan.verDetalle());
+        System.out.println(mazda.acerarFrenar(4000));
+        System.out.println(auto.equals(fecha));//no puede comparar no puede hacer cast
+        System.out.println(auto.equals(nissan));
+// se trata de polimorfismo: diferentes formas de hacer lo mismo
+
+        //principio de encapsulamiento que se confundo con el principio de ocultación
+        System.out.println("kilometros por litro = " + subaru.calcularConsumo(300, 0.6f));
+        System.out.println("kilometros por litro = " + subaru.calcularConsumo(300, 60));
+
+
+    }
+
+
+}
